ddc4cc2acdd70111c5da0542882de2bc6fa5de3c
pl465j <pl465j@att.com>
2021-08-26 16:41:29 +0530

    Added Vars to be accessed from Secrete and Config Map

--- app/SnowFlakeConnection.py
+++ app/SnowFlakeConnection.py
@@ -1,8 +1,17 @@
 from snowflake import connector
+import os
 
 
 def sfConnect():
-    cnx = connector.connect(account='jd98257.canada-central.azure',
-                            user='prashant887', password='prashantL@887',
-                            warehouse='COMPUTE_WH', database='DEMO_DB', schema='PUBLIC', role='ACCOUNTADMIN')
+    account = os.getenv('ACC')
+    user = os.getenv('USER')
+    password = os.getenv('PASSWD')
+    warehouse = os.getenv('WH')
+    database = os.getenv('DB')
+    schema = os.getenv('SCHEMA')
+    role = os.getenv('ROLE')
+
+    # print("user = [{}] , password=[{}]".format(user,password))
+    cnx = connector.connect(account=account.strip(),user=user.strip(), password=password.strip(),
+                            warehouse=warehouse.strip(), database=database.strip(), schema=schema.strip(), role=role.strip())
     return cnx
